0d4683d3b355ae0c350622df7568a739c028c998
HanStillLearning <farhantjahsmpn5@gmail.com>
2025-08-11 14:10:34 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // API URL Web App (Google Apps Script)
-const API_URL = "https://script.google.com/macros/s/AKfycbxjP8QK2wJysOwUOeE6wJd_3nvkQOuCuZHyFj1sYFfeNJnvayVTat5RGrm73VEcfU6Owg/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbyqyZ1rsJuzfiY1pAj2_mshOnX6fpye5MiiOcfahaluAGBSd_xFHUqLy4AqulEOwUEyaw/exec";
 
 // --- JSONP loader for GET (menghindari masalah CORS pada Apps Script) ---
 function loadData() {
